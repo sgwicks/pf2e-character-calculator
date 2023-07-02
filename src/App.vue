450c9418bdd332b81520186876a6612d6ea4bec3
sgwicks <samgwicks@gmail.com>
2023-07-02 16:36:46 +0100

    Player Info component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <PlayerInfo />
     </div>
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <main></main>
 </template>
+
+<script setup lang="ts">
+import PlayerInfo from './components/layout/PlayerInfo.vue'
+</script>
 
 <style scoped>
 header {
---
+++ src/components/form/SGInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <label>
+    {{ label }}
+    <input v-model="value" />
+  </label>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+  modelValue: string
+  label?: string
+}>()
+
+const emit = defineEmits(['update:modelValue'])
+
+const value = computed({
+  get: () => props.modelValue,
+  set: (val) => emit('update:modelValue', val)
+})
+</script>
+
+<style scoped>
+label {
+  display: flex;
+  flex-direction: column;
+  padding: 5px;
+}
+</style>
