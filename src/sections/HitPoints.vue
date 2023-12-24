25b754ac5897ff7bc4ab6f86e6d72be2427fd010
fodsgwicks <sam.wicks@fodmobilitygroup.com>
2023-12-24 08:56:53 +0000

    Allow updating health

--- src/sections/HitPoints.vue
+++ src/sections/HitPoints.vue
@@ -6,26 +6,78 @@
     <SGInput v-model="hp.dying" label="Dying" />
     <SGInput v-model="hp.wounded" label="Wounded" />
     <div style="width: 100%">
-      <SGInput v-model="hp.resistances" label="Resistances" />
-      <SGInput v-model="hp.immunities" label="Immunites" />
-      <SGInput v-model="hp.conditions" label="Conditions" />
+      <SGInput v-model="resistances" label="Resistances" />
+      <SGInput v-model="immunities" label="Immunites" />
+      <SGInput v-model="conditions" label="Conditions" />
+      <SGInput v-model="weaknesses" label="Weaknesses" />
     </div>
   </SGSection>
 </template>
 
 <script setup lang="ts">
-import { reactive, ref } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 import SGSection from '@/components/layout/SGSection.vue'
 import SGInput from '@/components/form/SGInput.vue'
 
-const hp = reactive({
+import { useCharacterStore } from '@/stores/character'
+import { storeToRefs } from 'pinia'
+import { cloneDeep, debounce } from 'lodash'
+import { patchHealth } from '@/api/health'
+
+const characterStore = useCharacterStore()
+
+const { syncApiCharacterDown } = characterStore
+const { character } = storeToRefs(characterStore)
+
+const hp = ref<CharacterHealth>({
   max: 0,
   current: 0,
   temporary: 0,
   dying: 0,
   wounded: 0,
-  resistances: '',
-  immunities: '',
-  conditions: ''
+  resistances: [],
+  immunities: [],
+  conditions: [],
+  weaknesses: []
+})
+
+const resistances = computed<string>({
+  get: () => hp.value.resistances.toString(),
+  set: (val) => (hp.value.resistances = val.split(','))
+})
+
+const immunities = computed<string>({
+  get: () => hp.value.immunities.toString(),
+  set: (val) => (hp.value.immunities = val.split(','))
+})
+
+const conditions = computed<string>({
+  get: () => hp.value.conditions.toString(),
+  set: (val) => (hp.value.conditions = val.split(','))
+})
+
+const weaknesses = computed<string>({
+  get: () => hp.value.weaknesses.toString(),
+  set: (val) => (hp.value.weaknesses = val.split(','))
+})
+
+const handleUpdateHealth = debounce(async () => {
+  if (!character.value) return
+  await patchHealth(character.value.id, hp.value)
+  syncApiCharacterDown(character.value.id)
+}, 1000)
+
+onMounted(() => {
+  if (character.value) {
+    hp.value = cloneDeep(character.value.health)
+  }
+
+  watch(
+    hp,
+    () => {
+      handleUpdateHealth()
+    },
+    { deep: true }
+  )
 })
 </script>
