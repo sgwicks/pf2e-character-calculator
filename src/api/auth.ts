fb8e17c493f0cfddc6cd158f24126064313c8583
Sam Wicks <60254658+sgwicks@users.noreply.github.com>
2024-03-09 16:20:14 +0000

    Auth (#11)
    * Refresh token on each request
    
    * Set refreshCutoff
    
    ---------
    
    Co-authored-by: fodsgwicks <sam.wicks@fodmobilitygroup.com>

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,8 +1,34 @@
-import client from '.'
+import axios from 'axios'
+import Cookies from 'js-cookie'
+
+import { globalRouter } from '@/routes/globalRouter'
+import { jwtDecode } from 'jwt-decode'
+
+const authClient = axios.create({
+  baseURL: import.meta.env.VITE_APP_BASE_URL + '/auth'
+})
+
+authClient.interceptors.response.use(
+  (response) => {
+    if (typeof response.headers.getAuthorization == 'function') {
+      const token = response.headers.getAuthorization()
+      if (token) {
+        Cookies.set('bearer', token.toString())
+      }
+    }
+
+    return response
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      globalRouter.router?.push('/')
+    }
+  }
+)
 
 const login = (username: string, password: string) => {
-  return client.post(
-    '/auth/login',
+  return authClient.post(
+    '/login',
     {},
     {
       auth: {
@@ -13,4 +39,44 @@ const login = (username: string, password: string) => {
   )
 }
 
-export { login }
+const refresh = async () => {
+  const token = Cookies.get('bearer')
+
+  if (!token) throw 'No token stored'
+
+  const expiry = jwtDecode(token).exp
+
+  if (!expiry) throw 'Invalid JWT'
+
+  const expiryMS = expiry * 1000
+
+  const expiresIn = expiryMS - Date.now()
+  const refreshCutoff = import.meta.env.VITE_APP_REFRESH_TIMEOUT_IN_MINUTES * 60 * 1000
+
+  if (expiresIn > refreshCutoff) {
+    return token.toString()
+  }
+
+  const response = await authClient.post(
+    '/refresh',
+    {},
+    { headers: { Authorization: `Bearer ${token}` } }
+  )
+
+  if (typeof response.headers.getAuthorization == 'function') {
+    const token = response.headers.getAuthorization()
+    if (token) {
+      Cookies.set('bearer', token.toString())
+      return token.toString()
+    }
+  }
+}
+
+const logout = async () => {
+  const token = Cookies.get('bearer')
+  authClient.post('/logout', {}, { headers: { Authorization: token } })
+  Cookies.remove('bearer')
+  globalRouter.router?.push('/')
+}
+
+export { login, refresh, logout }
