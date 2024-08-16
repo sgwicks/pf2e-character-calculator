f89020415e57a7be113efb40829ab5b0e446b6e4
sgwicks <samgwicks@gmail.com>
2024-08-16 12:55:30 +0100

    Setup sentry

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,11 @@ import { routes } from './routes'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
 
+import * as Sentry from '@sentry/vue'
+
 import App from './App.vue'
+
+const env = import.meta.env
 
 const router = createRouter({
   history: createWebHistory(),
@@ -16,6 +20,16 @@ const router = createRouter({
 })
 
 const app = createApp(App)
+
+Sentry.init({
+  app,
+  dsn: env.VITE_APP_SENTRY_DSN,
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  tracesSampleRate: 1,
+  tracePropagationTargets: ['localhost', env.VITE_APP_BASE_URL, env.VITE_APP_WEB_URL],
+  replaysSessionSampleRate: 1,
+  replaysOnErrorSampleRate: 1
+})
 
 // Set up pinia plugins
 const pinia = createPinia()
